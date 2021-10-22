fef5a454294c70b5775fee875eb66229e5f5e21a
victorjosecordero <vpencor@gmail.com>
2021-10-22 08:52:29 +0200

    holaa

---
+++ Funciones-1.py
@@ -0,0 +1,24 @@
+def primera_funcion(mensaje, usuario,tema):
+    return mensaje + usuario + tema
+
+v1 = primera('Hola mundo', ' teo',' clase de funciones')
+print(v1)
+
+# def segunda():
+#     v1 = 'hola'
+#     v2 = 'caracola'
+    
+#     return v1,v2
+
+# a1,a2 = segunda()
+# print(a1)
+# print(a2)
+
+
+# def tercera(uno=1):
+#     return uno
+
+# print(tercera('hola'))
+    
+# function_larga(*args, **kwargs)
+# function_larga(1,2,3,4,name='teo',hora='17',min='22')
